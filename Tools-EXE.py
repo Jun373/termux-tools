f42576077abb1e79f0ed7f23193d8cafd6e4d888
Jun373 <65720199+Jun373@users.noreply.github.com>
2021-02-25 22:24:45 +0700

    Add files via upload

---
+++ Tools-EXE.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import time
+
+os.system("clear")
+os.system("figlet Tools-EXE")
+print
+print("Author : UDIN | REDSEC")
+print("Team : Netsec farmework & ClayXploit")
+print("Youtube : REDSEC")
+print("Github : https://github.com/REDSEC")
+print("Boleh Recode Tapi jangan plagiatin sc nya")
+print
+print("[1] DDoS")
+print("[2] Brute Force")
+print("[3] Create Payload")
+print("[4] infect Virus-Maker")
+print("[5] Database Dump")
+print("[6] RED HAWK")
+print("[7] Crack Passwords")
+print("[8] NONTON BOKEP")
+print("[9] Hack Satelit :v")
+print("[10] Hack CCTV")
+print("[11] Virtex Gratis")
+print("[12] Cloudmare")
+print("[13] Bot-Deface")
+print("[00] Exit")
+print
+Pilih = raw_input('Pilih = ')
+if Pilih =="13":
+  os.system("apt-get install python")
+  os.system("apt-get install python-pip")
+  os.system("apt-get install git")
+  os.system("git clone https://github.com/Aryaalfahrezi010/bot-deface")
+  os.system("python ngebot/ngebot.py")
+if Pilih =="12":
+  os.system("apt-get install python2")
+  os.system("apt-get install figlet")
+  os.system("git clone https://github.com/MrH0wl/Cloudmare.git")
+  os.system("python Cloudmare/Cloudmare.py")
+if Pilih =="11":
+  os.system("apt-get install git")
+  os.system("apt-get install curl")
+  os.system("apt-get install figlet")
+  os.system("apt-get install ruby")
+  os.system("gem install lolcat")
+  os.system("git clone https://github.com/ArroKM/VIRTEX")
+  os.system("bash virtex/virtex.sh")
+if Pilih =="10":
+  os.system("apt-get install python3")
+  os.system("apt-get install git")
+  os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+  os.system("python Cam-Hackers/cam-hackers.py")
+if Pilih =="9":
+  os.system("apt-get install git")
+  os.system("apt-get install python2")
+  os.system("git clone https://github.com/MaulanaRyM/Heker")
+  os.system("python santuy/santuy.py")
+if Pilih =="1":
+  os.system("pip install python")
+  os.system("pip install python2")
+  os.system("pip install git")
+  os.system("git clone https://github.com/EH30/byte-ddos")
+  os.system("python byte-ddos/byte-ddos.py")
+elif Pilih =="2":
+  os.system("clear")
+  os.system("pip install hydra")
+  os.system("clear")
+  os.system("hydra -h")
+elif Pilih =="3":
+  os.system("clear")
+  os.system("pip install metasploit-fremawork")
+  os.system("clear")
+  os.system("msfconsole")
+elif Pilih =="4":
+  os.system("clear")
+  os.system("apt-get install python")
+  os.system("apt-get install git")
+  os.system("pip install lolcat")
+  os.system("git clone https://github.com/noob-hackers/infect")
+  os.system("bash infect/infect.sh")
+elif Pilih =="5":
+  os.system("clear")
+  os.system("pip install sqlmap")
+  os.system("clear")
+  os.system("sqlmap")
+elif Pilih =="6":
+  os.system("clear")
+  os.system("pip install python git php-curl php-xml")
+  os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+  os.system("php RED_HAWK/rhawk.php")
+elif Pilih =="7":
+  os.system("clear")
+  os.system("pip install hashcat")
+  os.system("clear")
+  os.system("hashcat -h")
+elif Pilih =="8":
+  os.system("clear")
+  os.system("figlet BERDOSAAA")
+  print("INSYAFLAH WAHAI MANUSIA")
+  print
+  print("Zbokep: https://zbokep321.com/")
+  print
+  print("Itubokep: https://itubokep.pro/")
+  print
+  print("BOCIL NO NONTON OKE :) UNTUK HIBURAN")
